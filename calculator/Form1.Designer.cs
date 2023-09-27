3989138354fd29a5a5cb0220b1a51808e82ab7a1
mason <mason@DESKTOP-1QJH63D>
2023-09-27 22:52:42 +0100

    Add project files.

---
+++ calculator/Form1.Designer.cs
@@ -0,0 +1,419 @@
+﻿namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtMain = new MaskedTextBox();
+            btnMmryClr = new Button();
+            btnMmryRcll = new Button();
+            btnMmryStr = new Button();
+            btnMmrySub = new Button();
+            btnMmryAdd = new Button();
+            btnBckSpc = new Button();
+            btnClrEntry = new Button();
+            btnClrAll = new Button();
+            btnNegation = new Button();
+            btnSqrRoot = new Button();
+            btnSeven = new Button();
+            btnNine = new Button();
+            btnPercent = new Button();
+            btnDivide = new Button();
+            btnEight = new Button();
+            btnFive = new Button();
+            btnFraction = new Button();
+            btnMultiply = new Button();
+            btnSix = new Button();
+            btnFour = new Button();
+            btnSubtract = new Button();
+            btnThree = new Button();
+            btnTwo = new Button();
+            btnOne = new Button();
+            btnEquals = new Button();
+            btnZero = new Button();
+            btnAddition = new Button();
+            btnDecimal = new Button();
+            SuspendLayout();
+            // 
+            // txtMain
+            // 
+            txtMain.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            txtMain.Location = new Point(12, 12);
+            txtMain.Name = "txtMain";
+            txtMain.ReadOnly = true;
+            txtMain.Size = new Size(241, 43);
+            txtMain.TabIndex = 0;
+            txtMain.Text = "0";
+            txtMain.TextAlign = HorizontalAlignment.Right;
+            // 
+            // btnMmryClr
+            // 
+            btnMmryClr.Location = new Point(12, 61);
+            btnMmryClr.Name = "btnMmryClr";
+            btnMmryClr.Size = new Size(43, 31);
+            btnMmryClr.TabIndex = 1;
+            btnMmryClr.Text = "MC";
+            btnMmryClr.UseVisualStyleBackColor = true;
+            // 
+            // btnMmryRcll
+            // 
+            btnMmryRcll.Location = new Point(61, 61);
+            btnMmryRcll.Name = "btnMmryRcll";
+            btnMmryRcll.Size = new Size(43, 31);
+            btnMmryRcll.TabIndex = 2;
+            btnMmryRcll.Text = "MR";
+            btnMmryRcll.UseVisualStyleBackColor = true;
+            // 
+            // btnMmryStr
+            // 
+            btnMmryStr.Location = new Point(110, 61);
+            btnMmryStr.Name = "btnMmryStr";
+            btnMmryStr.Size = new Size(43, 31);
+            btnMmryStr.TabIndex = 3;
+            btnMmryStr.Text = "MS";
+            btnMmryStr.UseVisualStyleBackColor = true;
+            // 
+            // btnMmrySub
+            // 
+            btnMmrySub.Location = new Point(208, 61);
+            btnMmrySub.Name = "btnMmrySub";
+            btnMmrySub.Size = new Size(43, 31);
+            btnMmrySub.TabIndex = 4;
+            btnMmrySub.Text = "M-";
+            btnMmrySub.UseVisualStyleBackColor = true;
+            // 
+            // btnMmryAdd
+            // 
+            btnMmryAdd.Location = new Point(159, 61);
+            btnMmryAdd.Name = "btnMmryAdd";
+            btnMmryAdd.Size = new Size(43, 31);
+            btnMmryAdd.TabIndex = 5;
+            btnMmryAdd.Text = "M+";
+            btnMmryAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnBckSpc
+            // 
+            btnBckSpc.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            btnBckSpc.Location = new Point(12, 98);
+            btnBckSpc.Name = "btnBckSpc";
+            btnBckSpc.Size = new Size(43, 31);
+            btnBckSpc.TabIndex = 6;
+            btnBckSpc.Text = "←";
+            btnBckSpc.UseVisualStyleBackColor = true;
+            // 
+            // btnClrEntry
+            // 
+            btnClrEntry.Location = new Point(61, 98);
+            btnClrEntry.Name = "btnClrEntry";
+            btnClrEntry.Size = new Size(43, 31);
+            btnClrEntry.TabIndex = 7;
+            btnClrEntry.Text = "CE";
+            btnClrEntry.UseVisualStyleBackColor = true;
+            // 
+            // btnClrAll
+            // 
+            btnClrAll.Location = new Point(110, 98);
+            btnClrAll.Name = "btnClrAll";
+            btnClrAll.Size = new Size(43, 31);
+            btnClrAll.TabIndex = 8;
+            btnClrAll.Text = "C";
+            btnClrAll.UseVisualStyleBackColor = true;
+            // 
+            // btnNegation
+            // 
+            btnNegation.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            btnNegation.Location = new Point(159, 98);
+            btnNegation.Name = "btnNegation";
+            btnNegation.Size = new Size(43, 31);
+            btnNegation.TabIndex = 9;
+            btnNegation.Text = "±";
+            btnNegation.UseVisualStyleBackColor = true;
+            // 
+            // btnSqrRoot
+            // 
+            btnSqrRoot.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            btnSqrRoot.Location = new Point(208, 98);
+            btnSqrRoot.Name = "btnSqrRoot";
+            btnSqrRoot.Size = new Size(43, 31);
+            btnSqrRoot.TabIndex = 10;
+            btnSqrRoot.Text = "√";
+            btnSqrRoot.UseVisualStyleBackColor = true;
+            // 
+            // btnSeven
+            // 
+            btnSeven.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnSeven.Location = new Point(12, 135);
+            btnSeven.Name = "btnSeven";
+            btnSeven.Size = new Size(43, 31);
+            btnSeven.TabIndex = 11;
+            btnSeven.Text = "7";
+            btnSeven.UseVisualStyleBackColor = true;
+            // 
+            // btnNine
+            // 
+            btnNine.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnNine.Location = new Point(110, 135);
+            btnNine.Name = "btnNine";
+            btnNine.Size = new Size(43, 31);
+            btnNine.TabIndex = 12;
+            btnNine.Text = "9";
+            btnNine.UseVisualStyleBackColor = true;
+            // 
+            // btnPercent
+            // 
+            btnPercent.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            btnPercent.Location = new Point(208, 135);
+            btnPercent.Name = "btnPercent";
+            btnPercent.Size = new Size(43, 31);
+            btnPercent.TabIndex = 13;
+            btnPercent.Text = "%";
+            btnPercent.UseVisualStyleBackColor = true;
+            // 
+            // btnDivide
+            // 
+            btnDivide.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            btnDivide.Location = new Point(159, 135);
+            btnDivide.Name = "btnDivide";
+            btnDivide.Size = new Size(43, 31);
+            btnDivide.TabIndex = 14;
+            btnDivide.Text = "÷";
+            btnDivide.UseVisualStyleBackColor = true;
+            // 
+            // btnEight
+            // 
+            btnEight.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnEight.Location = new Point(61, 135);
+            btnEight.Name = "btnEight";
+            btnEight.Size = new Size(43, 31);
+            btnEight.TabIndex = 15;
+            btnEight.Text = "8";
+            btnEight.UseVisualStyleBackColor = true;
+            // 
+            // btnFive
+            // 
+            btnFive.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnFive.Location = new Point(61, 172);
+            btnFive.Name = "btnFive";
+            btnFive.Size = new Size(43, 31);
+            btnFive.TabIndex = 16;
+            btnFive.Text = "5";
+            btnFive.UseVisualStyleBackColor = true;
+            // 
+            // btnFraction
+            // 
+            btnFraction.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            btnFraction.Location = new Point(208, 172);
+            btnFraction.Name = "btnFraction";
+            btnFraction.Size = new Size(43, 31);
+            btnFraction.TabIndex = 17;
+            btnFraction.Text = "⅟";
+            btnFraction.UseVisualStyleBackColor = true;
+            // 
+            // btnMultiply
+            // 
+            btnMultiply.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            btnMultiply.Location = new Point(159, 172);
+            btnMultiply.Name = "btnMultiply";
+            btnMultiply.Size = new Size(43, 31);
+            btnMultiply.TabIndex = 18;
+            btnMultiply.Text = "×";
+            btnMultiply.UseVisualStyleBackColor = true;
+            // 
+            // btnSix
+            // 
+            btnSix.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnSix.Location = new Point(110, 172);
+            btnSix.Name = "btnSix";
+            btnSix.Size = new Size(43, 31);
+            btnSix.TabIndex = 19;
+            btnSix.Text = "6";
+            btnSix.UseVisualStyleBackColor = true;
+            // 
+            // btnFour
+            // 
+            btnFour.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnFour.Location = new Point(12, 172);
+            btnFour.Name = "btnFour";
+            btnFour.Size = new Size(43, 31);
+            btnFour.TabIndex = 20;
+            btnFour.Text = "4";
+            btnFour.UseVisualStyleBackColor = true;
+            // 
+            // btnSubtract
+            // 
+            btnSubtract.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            btnSubtract.Location = new Point(159, 209);
+            btnSubtract.Name = "btnSubtract";
+            btnSubtract.Size = new Size(43, 31);
+            btnSubtract.TabIndex = 21;
+            btnSubtract.Text = "-";
+            btnSubtract.UseVisualStyleBackColor = true;
+            // 
+            // btnThree
+            // 
+            btnThree.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnThree.Location = new Point(111, 209);
+            btnThree.Name = "btnThree";
+            btnThree.Size = new Size(43, 31);
+            btnThree.TabIndex = 22;
+            btnThree.Text = "3";
+            btnThree.UseVisualStyleBackColor = true;
+            // 
+            // btnTwo
+            // 
+            btnTwo.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnTwo.Location = new Point(61, 209);
+            btnTwo.Name = "btnTwo";
+            btnTwo.Size = new Size(43, 31);
+            btnTwo.TabIndex = 23;
+            btnTwo.Text = "2";
+            btnTwo.UseVisualStyleBackColor = true;
+            // 
+            // btnOne
+            // 
+            btnOne.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnOne.Location = new Point(12, 209);
+            btnOne.Name = "btnOne";
+            btnOne.Size = new Size(43, 31);
+            btnOne.TabIndex = 24;
+            btnOne.Text = "1";
+            btnOne.UseVisualStyleBackColor = true;
+            // 
+            // btnEquals
+            // 
+            btnEquals.Font = new Font("Segoe UI", 20F, FontStyle.Bold, GraphicsUnit.Point);
+            btnEquals.Location = new Point(208, 209);
+            btnEquals.Name = "btnEquals";
+            btnEquals.Size = new Size(43, 68);
+            btnEquals.TabIndex = 25;
+            btnEquals.Text = "=";
+            btnEquals.UseVisualStyleBackColor = true;
+            // 
+            // btnZero
+            // 
+            btnZero.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnZero.Location = new Point(12, 246);
+            btnZero.Name = "btnZero";
+            btnZero.Size = new Size(92, 31);
+            btnZero.TabIndex = 26;
+            btnZero.Text = "0";
+            btnZero.UseVisualStyleBackColor = true;
+            // 
+            // btnAddition
+            // 
+            btnAddition.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            btnAddition.Location = new Point(159, 246);
+            btnAddition.Name = "btnAddition";
+            btnAddition.Size = new Size(43, 31);
+            btnAddition.TabIndex = 27;
+            btnAddition.Text = "+";
+            btnAddition.UseVisualStyleBackColor = true;
+            // 
+            // btnDecimal
+            // 
+            btnDecimal.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnDecimal.Location = new Point(111, 246);
+            btnDecimal.Name = "btnDecimal";
+            btnDecimal.Size = new Size(43, 31);
+            btnDecimal.TabIndex = 28;
+            btnDecimal.Text = ".";
+            btnDecimal.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(265, 290);
+            Controls.Add(btnDecimal);
+            Controls.Add(btnAddition);
+            Controls.Add(btnZero);
+            Controls.Add(btnEquals);
+            Controls.Add(btnOne);
+            Controls.Add(btnTwo);
+            Controls.Add(btnThree);
+            Controls.Add(btnSubtract);
+            Controls.Add(btnFour);
+            Controls.Add(btnSix);
+            Controls.Add(btnMultiply);
+            Controls.Add(btnFraction);
+            Controls.Add(btnFive);
+            Controls.Add(btnEight);
+            Controls.Add(btnDivide);
+            Controls.Add(btnPercent);
+            Controls.Add(btnNine);
+            Controls.Add(btnSeven);
+            Controls.Add(btnSqrRoot);
+            Controls.Add(btnNegation);
+            Controls.Add(btnClrAll);
+            Controls.Add(btnClrEntry);
+            Controls.Add(btnBckSpc);
+            Controls.Add(btnMmryAdd);
+            Controls.Add(btnMmrySub);
+            Controls.Add(btnMmryStr);
+            Controls.Add(btnMmryRcll);
+            Controls.Add(btnMmryClr);
+            Controls.Add(txtMain);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MaskedTextBox txtMain;
+        private Button btnMmryClr;
+        private Button btnMmryRcll;
+        private Button btnMmryStr;
+        private Button btnMmrySub;
+        private Button btnMmryAdd;
+        private Button btnBckSpc;
+        private Button btnClrEntry;
+        private Button btnClrAll;
+        private Button btnNegation;
+        private Button btnSqrRoot;
+        private Button btnSeven;
+        private Button btnNine;
+        private Button btnPercent;
+        private Button btnDivide;
+        private Button btnEight;
+        private Button btnFive;
+        private Button btnFraction;
+        private Button btnMultiply;
+        private Button btnSix;
+        private Button btnFour;
+        private Button btnSubtract;
+        private Button btnThree;
+        private Button btnTwo;
+        private Button btnOne;
+        private Button btnEquals;
+        private Button btnZero;
+        private Button btnAddition;
+        private Button btnDecimal;
+    }
+}
