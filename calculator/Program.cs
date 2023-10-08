b77b56d61687a382e46f5afbef9aafc0f117684b
Matthew Mann <mason4653@gmail.com>
2023-10-08 15:34:58 +0100

    Modified min size to better suit lower resolution monitors

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -11,7 +11,10 @@ namespace calculator
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.Run(new Form1());
+
         }
     }
 }
